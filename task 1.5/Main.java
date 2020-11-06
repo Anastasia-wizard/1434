8d4a895066d954b4ac43f9ad42c1fb01f10902b9
Anastasia-wizard <73994530+Anastasia-wizard@users.noreply.github.com>
2020-11-06 16:11:26 +0300

    Create Main.java

---
+++ task 1.5/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double a = scan.nextDouble();
+        double b;
+        b = ((double)9/5) * a + 32;
+        System.out.println(b);
+
+        
+    }
+}
