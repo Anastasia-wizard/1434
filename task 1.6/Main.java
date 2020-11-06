f2cad1bd20059418934b50f68460edfb96787396
Anastasia-wizard <73994530+Anastasia-wizard@users.noreply.github.com>
2020-11-06 16:18:10 +0300

    Create Main.java

---
+++ task 1.6/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double a = scan.nextDouble();
+        double b;
+        b = a*0.15 + a;
+        System.out.println(b);
