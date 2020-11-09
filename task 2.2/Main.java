86ad445ece5af96a3159a331a13a5f4d8c28e04e
Anastasia-wizard <73994530+Anastasia-wizard@users.noreply.github.com>
2020-11-09 16:56:02 +0300

    Create Main.java

---
+++ task 2.2/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        int numbers[] = {1,2,3,4,5,6,7,8,9,10};
+
+
+
+        for (int j = 0; j < numbers.length; j++) {
+            int next = 0;
+            int b = 0;
+            b = b + j;
+
+
+            for (int i = 0; i < numbers.length; i++) {
+
+                next = next + b;
+                System.out.print(next);
+                System.out.print(" ");
+                
+            }
+            System.out.println();
+        }  //я вообще не поняла как это сделала, но оно работает...
