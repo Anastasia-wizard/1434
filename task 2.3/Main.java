99d09942943bb8904182a46137797a42a870b45f
Anastasia-wizard <73994530+Anastasia-wizard@users.noreply.github.com>
2020-11-09 17:14:18 +0300

    Create Main.java

---
+++ task 2.3/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+       int a = 0;
+        for (int j = 0; j < 10; j++) {
+            a = a+1;
+
+
+
+            for (int i = 0; i < a; i++) {
+                System.out.print("8");
+
+            }
+            System.out.println( );
+        }
+        } 
