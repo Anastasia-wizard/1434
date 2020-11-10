521c7cac084c81ac9b57fc99cb33de883620126e
Anastasia-wizard <73994530+Anastasia-wizard@users.noreply.github.com>
2020-11-10 20:39:06 +0300

    Create Main.java

---
+++ task 3.4/Main.java
@@ -0,0 +1,22 @@
+/*
+Задание: Сделайте функцию, которая параметрами принимает 2 числа. Если их сумма больше 10 - пусть функция вернет true, а если нет - false.
+*/
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        if ((a+b)>10) {
+            System.out.println("true");
+        }
+            else
+        {
+            System.out.println("false");
+        }
+
+        //напишите тут ваш код
+
+    }
+}
