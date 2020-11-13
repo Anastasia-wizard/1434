6d89a8083b3ca9c7874fcabf49ee3fa494b46811
Anastasia-wizard <73994530+Anastasia-wizard@users.noreply.github.com>
2020-11-13 10:05:12 +0300

    Update Main.java

--- task 2.11/Main.java
+++ task 2.11/Main.java
@@ -1,16 +1,20 @@
+//Дан массив с числами. Узнайте сколько элементов с начала массива надо сложить, чтобы в сумме получилось больше 10-ти.
+
 public class Main {
     public static void main(String[] args) {
-       int arr[] = {2, 3, 1, 4, 23, 341,77783};
-       int sum = 0;
+        int arr[] = {2, 3, 1, 4, 23, 341,77783};
+        int sum = 0;
+        int count=0;
         for (int i = 0; i < arr.length; i++) {
             sum = sum + arr[i];
-            while (sum > 10);
+            while (sum >= 10)
+            {
+                count++;
+                break;
+            }
 
-            System.out.println(arr[i]);
-            
         }
-
-
+        System.out.println(count);
 
 
     }
