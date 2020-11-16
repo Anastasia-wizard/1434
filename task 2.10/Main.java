d73d84123ab5c1b8d031d7105be31dbb3f7c0298
Anastasia-wizard <73994530+Anastasia-wizard@users.noreply.github.com>
2020-11-16 12:11:00 +0300

    Update Main.java

--- task 2.10/Main.java
+++ task 2.10/Main.java
@@ -1,17 +1,17 @@
+/*
+Задание: Дан массив с числами. Проверьте, есть ли в нем два одинаковых числа подряд. Если есть - выведите 'да', а если нет - выведите 'нет'.
+*/
 public class Main {
     public static void main(String[] args) {
-        int [] array = {1, 2, 4, 5, 7, 1, 2, 2};
+        int [] array = {1, 2, 4, 5, 7, 1, 2};
+        boolean flag = false;
         //напишите тут ваш код
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] == array [i+1])
-            {
-                System.out.println("да");
-            }
-            else
-            {
-                System.out.println("нет");
+        for (int i = 0; i < array.length-1; i++) {
+            if (array[i] == array[i + 1]) flag = true;
+        }
+        if (flag)  System.out.println("да");
+
+            else System.out.println("нет");
             }
 
         }
-    }
-}
