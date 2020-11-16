38d9af21c24c7f7880687654f2759597f4b99fad
Anastasia-wizard <73994530+Anastasia-wizard@users.noreply.github.com>
2020-11-16 17:46:54 +0300

    Create Main.java

---
+++ task_5.2/Main.java
@@ -0,0 +1,52 @@
+/*
+Задача: Программа определяет, какая семья (фамилию) живёт в доме с указанным номером.
+Новая задача: Программа должна работать не с номерами домов, а с городами:
+Пример ввода:
+Москва
+Ивановы
+Киев
+Петровы
+Лондон
+Абрамовичи
+Лондон
+Пример вывода:
+Абрамовичи */
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/*
+Модернизация ПО
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<String> list = new ArrayList<>();
+        while (true) {
+            String family = scanner.nextLine();
+            if (family.isEmpty()) {
+                break;
+            }
+
+            list.add(family);
+        }
+
+        // Read the house number
+        String city = scanner.nextLine();
+
+        for (int i = 0; i < list.size(); i++) {
+
+
+            if (!city.isEmpty()) {
+
+                String familyName = list.get(city.indexOf(i));
+                System.out.println(familyName);
+
+            }
+        }
+    }
+}
