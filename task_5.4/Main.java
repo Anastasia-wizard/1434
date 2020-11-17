ed79a7a66bdcaddf0761ec279bea998a10a2a224
Anastasia-wizard <73994530+Anastasia-wizard@users.noreply.github.com>
2020-11-17 21:22:37 +0300

    Create Main.java

---
+++ task_5.4/Main.java
@@ -0,0 +1,54 @@
+/*
+1. Ввести путь к файлу с консоли.
+2. Прочитать из него набор чисел.
+3. Вывести на консоль только четные, отсортированные по возрастанию.
+Пример ввода:
+5
+8
+-2
+11
+3
+-5
+2
+10
+Пример вывода:
+-2
+2
+8
+10
+*/
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> num = new ArrayList<>();
+
+        try {
+            FileReader reader = new FileReader("C:/java/text1.txt");
+            Scanner scan = new Scanner(reader);
+            String c = "";
+            int a;
+
+            while (scan.hasNextLine()){
+
+            c = scan.nextLine();
+            a = Integer.parseInt(c);
+            if (a%2 == 0)  num.add(a);
+            
+            }
+
+            Collections.sort(num);
+            System.out.print(num);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    }
